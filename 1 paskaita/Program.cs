bd5d84e9143c3b990ce09da226427eaccad67123
getNewUser <vilmantas.neviera@gmail.com>
2024-10-07 21:48:30 +0300

    initial commit

---
+++ 1 paskaita/Program.cs
@@ -0,0 +1,16 @@
+﻿int age = 35;
+
+Console.WriteLine("Man yra: " + age + " metai.");
+
+int sum = 10 - 5;
+
+Console.WriteLine(sum);
+
+int anotherSum = age - sum;
+
+Console.WriteLine(anotherSum);
+
+string name = "Vilmantas";
+int myAge = 26;
+
+Console.WriteLine("Mano vardas: " + name + ", man yra: " + myAge + " metų(ai)");
